c7243160648a5c8d7f2c21595f7a46003313b826
Shazia Anwar <178968634+shazia-anwar@users.noreply.github.com>
2026-04-24 12:45:33 +0530

    Finishing Touches

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import streamlit as st
 from Src.Screens.home_screen import home_screen
 from Src.Screens.teacher_screen import teacher_screen
 from Src.Screens.student_screen import student_screen
+from Src.Components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
+    st.set_page_config(
+        page_title='SnapClass - Making Attendance faster using AI',
+        page_icon= "https://i.ibb.co/YTYGn5qV/logo.png"
+    )
+    
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
     
@@ -17,5 +23,14 @@ def main():
         
         case None:
             home_screen()
+    
+    join_code = st.query_params.get('join-code')
+    
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
 
 main()
